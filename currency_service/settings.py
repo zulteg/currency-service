9c7eb9aa946c92457dbf3795ca7a2d41d34010eb
Taras Kolomoiets <zulteg@gmail.com>
2019-09-25 18:44:49 +0300

    configure

--- currency_service/settings.py
+++ currency_service/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'currency_service.urls'
